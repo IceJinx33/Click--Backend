2dc7daa4887f1d48619e4aed4f003da763eda5d8
wanqing0820 <57580398+wanqing0820@users.noreply.github.com>
2019-11-30 01:58:19 +0800

    Update db.py

--- Click/src/db.py
+++ Click/src/db.py
@@ -71,3 +71,90 @@ class Interest(db.Model):
             'id': self.id,
             'interest_name': self.interest_name
         }
+    
+    # updating in the branch
+    class User(db.Model):
+    __tablename__ = 'Users_Table'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, nullable = False)
+    netid = db.Column(db.String, nullable = False)
+    year = db.Column(db.String, nullable = False)
+    school = db.Column(db.String, nullable = False)
+    interests = db.relationship('Interest', secondary = association_table_user_interest, back_populates = 'users')
+    met_users = db.relationship('User', secondary = association_table_met_users, back_populates = 'met_users')
+    requests = db.relationship('Request', secondary=association_table_met_users, back_populates='requests')
+
+
+    def __init__(self, **kwargs):
+        self.name= kwargs.get('name', '')
+        self.netid = kwargs.get('netid', '')
+        self.year = kwargs.get('year', '')
+        self.school = kwargs.get('school', '')
+
+    def serialize_long(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'netid': self.netid,
+            'year': self.year,
+            'school': self.school,
+            'interests': [i.serialize() for i in self.interests],
+            'met_users': [m.serialize() for m in self.met_users]
+        }
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'netid': self.netid,
+            'year': self.year,
+            'school': self.school,
+            'interests': [i.serialize() for i in self.interests]
+        }
+
+
+    class Interest(db.Model):
+        __tablename__ = 'Interests_Table'
+        id = db.Column(db.Integer, primary_key = True)
+        interest_name = db.Column(db.String, nullable = False)
+        users = db.relationship('User', secondary = association_table_user_interest, back_populates = 'interests')
+
+        def __init__(self, **kwargs):
+        self.interest_name= kwargs.get('interest_name', '')
+
+        def serialize_long(self):
+        return {
+            'id': self.id,
+            'interest_name': self.interest_name,
+            'users': [u.serialize() for u in self.users]
+            }
+
+        def serialize(self):
+        return {
+            'id': self.id,
+            'interest_name': self.interest_name
+            }
+    
+    class Request(db.Model):
+        __tablename__='Request_Table'
+        id = db.Column(db.Integer, primary_key=True)
+        sender = db.relationship('User', secondary=association_table_met_users, back_populates='requests')
+        receiver = db.relationship('User', secondary=association_table_met_users, back_populates='requests')
+        # User must be updated to have a field called requests
+        accepted = db.Column(db.Boolean, nullable = False)
+
+        def __init__(self, **kwargs):
+            self.accepted = False
+        
+        def serialize(self):
+            return {
+            'id': self.id,
+            'sender': self.sender.serialize(),
+            'receiver': self.receiver.serialize(),
+            'accepted': self.accepted
+            }
+
+
+
+    
+    
