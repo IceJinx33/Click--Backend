e6b9b4f6a2bf26bbd9454575801d8da38766eb32
Aliva Das <alivadas2@gmail.com>
2019-12-06 21:24:53 -0500

    Extra routes added
    Routes for friend recommendations for a user, sending friend requests, accepting or rejecting a friend request added to app.py. Also db.py is updates to model this changes.

--- Click/src/db.py
+++ Click/src/db.py
@@ -2,16 +2,22 @@ import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-association_table_met_users = db.Table('association_met_users', db.Model.metadata,
-    db.Column('user_id', db.Integer, db.ForeignKey('Users_Table.id')),
-    db.Column('user_id', db.Integer, db.ForeignKey('Users_Table.id'))
-)
 
 association_table_user_interest = db.Table('association_user_interest', db.Model.metadata,
     db.Column('interest_id', db.Integer, db.ForeignKey('Interests_Table.id')),
     db.Column('user_id', db.Integer, db.ForeignKey('Users_Table.id'))
 )
-    
+
+association_table_request_send = db.Table('association_request_send', db.Model.metadata,
+    db.Column('request_id', db.Integer, db.ForeignKey('Requests_Table.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('Users_Table.id'))
+)
+
+association_table_request_rec = db.Table('association_request_rec', db.Model.metadata,
+    db.Column('request_id', db.Integer, db.ForeignKey('Requests_Table.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('Users_Table.id'))
+)
+
 class User(db.Model):
     __tablename__ = 'Users_Table'
     id = db.Column(db.Integer, primary_key = True)
@@ -20,15 +26,16 @@ class User(db.Model):
     year = db.Column(db.String, nullable = False)
     school = db.Column(db.String, nullable = False)
     interests = db.relationship('Interest', secondary = association_table_user_interest, back_populates = 'users')
-    met_users = db.relationship('User', secondary = association_table_met_users, back_populates = 'met_users')
-    requests = db.relationship('Request', secondary=association_table_met_users, back_populates='requests')
-
+    met_users = db.relationship('Friend', cascade = 'delete')
+    requests_sent = db.relationship('Request', secondary = association_table_request_send, back_populates = 'sender')
+    requests_rec = db.relationship('Request', secondary = association_table_request_rec, back_populates = 'receiver')
 
     def __init__(self, **kwargs):
         self.name= kwargs.get('name', '')
         self.netid = kwargs.get('netid', '')
         self.year = kwargs.get('year', '')
         self.school = kwargs.get('school', '')
+        self.met_users = []
 
     def serialize_long(self):
         return {
@@ -51,48 +58,64 @@ class User(db.Model):
             'interests': [i.serialize() for i in self.interests]
         }
 
-class Interest(db.Model):
-        __tablename__ = 'Interests_Table'
-        id = db.Column(db.Integer, primary_key = True)
-        interest_name = db.Column(db.String, nullable = False)
-        users = db.relationship('User', secondary = association_table_user_interest, back_populates = 'interests')
 
-        def __init__(self, **kwargs):
+class Interest(db.Model):
+    __tablename__ = 'Interests_Table'
+    id = db.Column(db.Integer, primary_key = True)
+    interest_name = db.Column(db.String, nullable = False)
+    users = db.relationship('User', secondary = association_table_user_interest, back_populates = 'interests')
+
+    def __init__(self, **kwargs):
         self.interest_name= kwargs.get('interest_name', '')
 
-        def serialize_long(self):
+    def serialize_long(self):
         return {
             'id': self.id,
             'interest_name': self.interest_name,
             'users': [u.serialize() for u in self.users]
-            }
+        }
 
-        def serialize(self):
+    def serialize(self):
         return {
             'id': self.id,
             'interest_name': self.interest_name
-            }
-    
+        }
+
+class Friend(db.Model):
+    __tablename__='Friends_Table'
+    id = db.Column(db.Integer, primary_key=True)
+    netid = db.Column(db.String, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('Users_Table.id'), nullable = False)
+
+    def __init__(self, **kwargs):
+        self.netid = kwargs.get('netid')
+        self.user_id = kwargs.get('user_id')
+
+    def serialize(self):
+        user = User.query.filter_by(netid=self.netid).first()
+        return {
+            'id': user.id,
+            'name': user.name,
+            'netid': user.netid,
+            'year': user.year,
+            'school': user.school,
+            'interests': [i.serialize() for i in user.interests]
+        }
+
 class Request(db.Model):
-        __tablename__='Request_Table'
-        id = db.Column(db.Integer, primary_key=True)
-        sender = db.relationship('User', secondary=association_table_met_users, back_populates='requests')
-        receiver = db.relationship('User', secondary=association_table_met_users, back_populates='requests')
-        # User must be updated to have a field called requests
-        accepted = db.Column(db.Boolean, nullable = False)
+    __tablename__='Requests_Table'
+    id = db.Column(db.Integer, primary_key=True)
+    sender = db.relationship('User', secondary = association_table_request_send, back_populates = 'requests_sent')
+    receiver = db.relationship('User', secondary = association_table_request_rec, back_populates = 'requests_rec')
+    accepted = db.Column(db.Boolean, nullable = False)
 
-        def __init__(self, **kwargs):
-            self.accepted = False
-        
-        def serialize(self):
-            return {
-            'id': self.id,
-            'sender': self.sender.serialize(),
-            'receiver': self.receiver.serialize(),
-            'accepted': self.accepted
-            }
+    def __init__(self, **kwargs):
+        self.accepted = kwargs.get('accepted', False)
 
-
-
-    
-    
+    def serialize(self):
+        return {
+        'id': self.id,
+        'sender_netid': [s.netid for s in self.sender],
+        'receiver_netid': [r.netid for r in self.receiver],
+        'accepted': self.accepted
+        }
